9c9da4cd01ed23c19621aabc339129c2240cdb25
MrRob <mrrob9403@gmail.com>
2024-02-02 15:36:34 +0100

    initial

---
+++ CarrierLookUp/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarrierLookUp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string s)
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
